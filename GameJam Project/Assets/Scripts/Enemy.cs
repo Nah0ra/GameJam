3b37cc9672bc2fb0f9eb55986192885d3066ea20
BlitzBlender <nah0ra@outlook.com>
2023-12-03 13:09:44 +0100

    - commit

--- GameJam Project/Assets/Scripts/Enemy.cs
+++ GameJam Project/Assets/Scripts/Enemy.cs
@@ -39,13 +39,21 @@ public class Enemy : MonoBehaviour
         {
             agent.destination = Goal.position;
             agent.transform.LookAt(Player.position);
+            animator.SetBool("IsWalking", true);
         }
         else if (gameObject.transform.tag == "Elite")
         {
             Player = GameObject.FindWithTag("Player").transform;
             agent.destination = Player.position;
             agent.transform.LookAt(Player.position);
+            animator.SetBool("IsWalking", true);
         }
     }
 
+    private void OnCollisionEnter(Collision other) {
+        if (other.transform.tag == "Player")
+        {
+            animator.Play("Slash");
+        }
+    }
 }

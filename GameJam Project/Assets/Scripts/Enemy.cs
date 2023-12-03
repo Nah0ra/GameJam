c732b6f930b30d63957b439bc26dca03fde26e56
BlitzBlender <nah0ra@outlook.com>
2023-12-03 06:42:01 +0100

    - Enemy Spawner fixed

--- GameJam Project/Assets/Scripts/Enemy.cs
+++ GameJam Project/Assets/Scripts/Enemy.cs
@@ -9,10 +9,21 @@ public class Enemy : MonoBehaviour
     private Transform Goal;
     private NavMeshAgent agent;
 
+    public int EnemyHealth;
+
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         Goal = GameObject.FindWithTag("Goal").transform;
+    }
+
+    public void TakeDamage()
+    {
+        EnemyHealth--;
+        if (EnemyHealth == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void Update() 

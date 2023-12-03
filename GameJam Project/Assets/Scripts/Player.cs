9675f2526f637e0ef5564a20c0fe8f6c0f5e7a56
BlitzBlender <nah0ra@outlook.com>
2023-12-03 05:45:39 +0100

    - Fixed player and holsters

--- GameJam Project/Assets/Scripts/Player.cs
+++ GameJam Project/Assets/Scripts/Player.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
     private GameObject CinematicCutscene;
 
-    [SerializeField]
     private GameObject PlayerCam;
+
+    private GameObject Gun;
+
+    private GameObject Hammer;
+
+    private GameObject GunHolster;
+
+    private GameObject HammerHolster;
 
     private void Start() 
     {
+        Gun = GameObject.Find("Pistol");
+        Hammer = GameObject.Find("Hammer");
+        GunHolster = GameObject.Find("GunHolster");
+        HammerHolster = GameObject.Find("HammerHolster");
         CinematicCutscene = GameObject.Find("CinematicCutscene");
         PlayerCam = GameObject.Find("Main Camera");
         PlayerCam.SetActive(false);
         StartCoroutine(SwitchCam());
+    }
+
+    public void ReturnToHolster()
+    {
+        Gun.transform.position = GunHolster.transform.position;
+        Hammer.transform.position = HammerHolster.transform.position;
     }
 
 
